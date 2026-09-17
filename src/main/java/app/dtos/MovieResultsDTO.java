b779bd6cd05fd3f53abfcfdb40055f4c61ca2930
Eligoon <derpepind117@live.dk>
2026-09-17 13:34:04 +0200

    MovieResultsDTO made

--- src/main/java/app/dtos/MovieResultsDTO.java
+++ src/main/java/app/dtos/MovieResultsDTO.java
@@ -1,10 +1,7 @@
 package app.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
